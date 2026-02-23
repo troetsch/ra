e46b7b726e0719f5a11c50953651788e3a7685e4
“Carlos <carlos.troetsch@gmail.com>
2026-02-23 18:59:44 +0100

    Add new file in python

---
+++ disk.py
@@ -0,0 +1,7 @@
+import shutil
+ 
+total, used, free = shutil.disk_usage("/")
+ 
+print("Total:", total // (1024**3), "GB")
+print("Used:", used // (1024**3), "GB")
+print("Free:", free // (1024**3), "GB")
