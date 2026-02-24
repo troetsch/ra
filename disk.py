6888a81161ec345f859176b4e0f085fc1dbde22e
troetsch <carlos.troetsch@gmail.com>
2026-02-24 11:27:01 +0100

    Add hostname and IP address retrieval to disk.py

--- disk.py
+++ disk.py
@@ -1,11 +1,24 @@
 ######################################
-# shutil                             #
+# shutil + socket                    #
 ######################################
 
 import shutil
- 
+import socket
+
+# Disk usage
 total, used, free = shutil.disk_usage("/")
- 
+
+# Hostname
+hostname = socket.gethostname()
+
+# IP address
+try:
+    ip_address = socket.gethostbyname(hostname)
+except socket.gaierror:
+    ip_address = "Unable to resolve IP"
+
+print("Hostname:", hostname)
+print("IP Address:", ip_address)
 print("Total:", total // (1024**3), "GB")
 print("Used:", used // (1024**3), "GB")
 print("Free:", free // (1024**3), "GB")
